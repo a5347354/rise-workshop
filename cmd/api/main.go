28ac5008f216897adc5532b207f6a9cac56078f4
ApolloLuke <a5347354@gmail.com>
2023-05-05 10:15:07 +0800

    add nostr client

---
+++ cmd/api/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/nbd-wtf/go-nostr"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	wsUrl := "wss://relay.nekolicio.us/"
+	pb := "privatekey"
+	pub := "publickey"
+	relay, err := nostr.RelayConnect(context.Background(), wsUrl)
+	defer relay.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	e := nostr.Event{
+		PubKey:    pub,
+		CreatedAt: nostr.Now(),
+		Kind:      31337,
+		Tags: nostr.Tags{
+			nostr.Tag{
+				"d",
+				"b07v7s2ic0haospgmeg73i",
+			},
+			nostr.Tag{
+				"media",
+				"https://media.zapstr.live:3118/d91191e30e00444b942c0e82cad470b32af171764c2275bee0bd99377efd4075/naddr1qqtxyvphwcmhxvnfvvcxsct0wdcxwmt9vumnx6gzyrv3ry0rpcqygju59s8g9jk5wzej4ut3wexzyad7uz7ejdm7l4q82qcyqqq856g4xnp7j",
+				"http",
+			},
+			nostr.Tag{
+				"p",
+				"d91191e30e00444b942c0e82cad470b32af171764c2275bee0bd99377efd4075",
+				"Host",
+			},
+			nostr.Tag{
+				"p",
+				"fa984bd7dbb282f07e16e7ae87b26a2a7b9b90b7246a44771f0cf5ae58018f52",
+				"Guest",
+			},
+			nostr.Tag{
+				"c",
+				"Podcast",
+			},
+			nostr.Tag{
+				"price",
+				"402",
+			},
+			nostr.Tag{
+				"cover",
+				"https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_nologo400/36291377/36291377-1673187804611-64b4f8e9f1687.jpg",
+			},
+			nostr.Tag{
+				"subject",
+				"Nostrovia | The Pablo Episode",
+			},
+		},
+		Content: "Nostrovia | The Pablo Episode\n\nhttps://s3-us-west-2.amazona",
+	}
+	e.ID = e.GetID()
+	e.Sign(pb)
+	_, err = e.CheckSignature()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(e.ID)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	status, err := relay.Publish(ctx, e)
+	if err != nil {
+		logrus.Panic(err)
+	}
+	if status != nostr.PublishStatusSucceeded {
+		logrus.Panic(status)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/a5347354/rise-workshop
+
+go 1.20
+
+require (
+	github.com/nbd-wtf/go-nostr v0.17.1
+	github.com/sirupsen/logrus v1.9.0
+)
+
+require (
+	github.com/SaveTheRbtz/generic-sync-map-go v0.0.0-20220414055132-a37292614db8 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.0.1 // indirect
+	github.com/decred/dcrd/crypto/blake256 v1.0.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/stretchr/testify v1.8.2 // indirect
+	golang.org/x/exp v0.0.0-20221106115401-f9659909a136 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+)
