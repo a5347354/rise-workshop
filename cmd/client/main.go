0d95d348e2eed5384cc646d86531fae506beeb66
ApolloLuke <a5347354@gmail.com>
2023-05-29 10:21:44 +0800

    fix wrong dependency injection

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/a5347354/rise-workshop/internal/client/delivery"
 	"github.com/a5347354/rise-workshop/internal/client/usecase"
-	"github.com/a5347354/rise-workshop/internal/event/store/postgres"
+	"github.com/a5347354/rise-workshop/internal/event/store/pubsub"
 	"github.com/a5347354/rise-workshop/pkg"
 
 	"strings"
@@ -25,7 +25,7 @@ func main() {
 			pkg.NewRouter,
 			pkg.NewPostgresClient,
 
-			postgres.NewEventStore,
+			pubsub.NewEventStore,
 			usecase.NewClient,
 		),
 		fx.Invoke(
