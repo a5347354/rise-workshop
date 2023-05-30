44afc7031de934aaffa9d3d6844f4ea4d7947490
ApolloLuke <a5347354@gmail.com>
2023-05-30 16:18:27 +0800

    fix notification concurrent issue

--- internal/relay/delivery/notification.go
+++ internal/relay/delivery/notification.go
@@ -20,15 +20,21 @@ func NewNotification(m *melody.Melody) relay.Notification {
 	return &Notification{m, make(map[string]*melody.Session), &sync.RWMutex{}}
 }
 
-func (n Notification) Subscribe(ctx context.Context, id string, s *melody.Session) {
+func (n *Notification) Subscribe(ctx context.Context, id string, s *melody.Session) {
+	n.Lock()
+	defer n.Unlock()
 	n.sessions[id] = s
 }
 
-func (n Notification) UnSubscribe(ctx context.Context, id string) {
+func (n *Notification) UnSubscribe(ctx context.Context, id string) {
+	n.Lock()
+	defer n.Unlock()
 	delete(n.sessions, id)
 }
 
-func (n Notification) Broadcast(ctx context.Context, msg []byte) {
+func (n *Notification) Broadcast(ctx context.Context, msg []byte) {
+	n.RLock()
+	defer n.RUnlock()
 	for _, session := range n.sessions {
 		err := n.m.BroadcastOthers(msg, session)
 		logrus.Warn(err)
