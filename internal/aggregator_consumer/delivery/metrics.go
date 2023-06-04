ed658f3d9494ad812f541f343108754e21c247ea
ApolloLuke <a5347354@gmail.com>
2023-06-04 16:10:46 +0800

    add metrics to consumer

---
+++ internal/aggregator_consumer/delivery/metrics.go
@@ -0,0 +1,58 @@
+package delivery
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"time"
+)
+
+type Metrics interface {
+	SuccessTotal(lvs ...string)
+	FailTotal(lvs ...string)
+	ProcessDuration(t time.Time, lvs ...string)
+}
+
+type metrics struct {
+	successTotal    *prometheus.CounterVec
+	failTotal       *prometheus.CounterVec
+	processDuration *prometheus.HistogramVec
+}
+
+func NewConsumerMetrics() Metrics {
+	return &metrics{
+		successTotal: promauto.NewCounterVec(
+			prometheus.CounterOpts{
+				Name: "success_total",
+				Help: "How many processed success",
+			},
+			[]string{},
+		),
+		failTotal: promauto.NewCounterVec(
+			prometheus.CounterOpts{
+				Name: "fail_total",
+				Help: "How many processed fail, partitioned by type and stage.",
+			},
+			[]string{"stage"},
+		),
+		processDuration: promauto.NewHistogramVec(
+			prometheus.HistogramOpts{
+				Name:    "processed_duration_second",
+				Help:    "The processed latencies in second",
+				Buckets: prometheus.DefBuckets,
+			},
+			[]string{},
+		),
+	}
+}
+
+func (m *metrics) SuccessTotal(lvs ...string) {
+	m.successTotal.WithLabelValues(lvs...).Inc()
+}
+
+func (m *metrics) FailTotal(lvs ...string) {
+	m.failTotal.WithLabelValues(lvs...).Inc()
+}
+
+func (m *metrics) ProcessDuration(t time.Time, lvs ...string) {
+	m.processDuration.WithLabelValues(lvs...).Observe(float64(time.Since(t)) / float64(time.Second))
+}
--- internal/aggregator_consumer/usecase/aggregator_usecase.go
+++ internal/aggregator_consumer/usecase/aggregator_usecase.go
@@ -3,8 +3,10 @@ package usecase
 import (
 	"github.com/a5347354/rise-workshop/internal"
 	"github.com/a5347354/rise-workshop/internal/aggregator_consumer"
+	"github.com/a5347354/rise-workshop/internal/aggregator_consumer/delivery"
 	"github.com/a5347354/rise-workshop/internal/event"
 	"github.com/a5347354/rise-workshop/pkg"
+	"time"
 
 	"context"
 	"encoding/json"
@@ -18,15 +20,17 @@ import (
 type consumerUsecase struct {
 	eStore       event.Store
 	pubsubClient pkg.PubsubClient
+	metrics      delivery.Metrics
 }
 
-func NewConsumer(eStore event.Store, pubsubClient pkg.PubsubClient) consumer.Usecase {
-	return &consumerUsecase{eStore, pubsubClient}
+func NewConsumer(eStore event.Store, pubsubClient pkg.PubsubClient, metrics delivery.Metrics) consumer.Usecase {
+	return &consumerUsecase{eStore, pubsubClient, metrics}
 }
 
 func (u consumerUsecase) Consume(ctx context.Context) error {
 	var mu sync.Mutex
 	return u.pubsubClient.Consume(ctx, func(ctx context.Context, msg *pubsub.Message) {
+		t := time.Now()
 		msg.Ack()
 		mu.Lock()
 		defer mu.Unlock()
@@ -34,9 +38,13 @@ func (u consumerUsecase) Consume(ctx context.Context) error {
 		fmt.Printf("Got message: %q\n", string(msg.Data))
 		if err := json.Unmarshal(msg.Data, &event); err != nil {
 			logrus.Warn(fmt.Errorf("unmarshal fail: %w", err))
+			u.metrics.FailTotal("consume")
 		}
 		if err := u.eStore.Insert(ctx, event); err != nil {
+			u.metrics.FailTotal("consume")
 			logrus.Warn(fmt.Errorf("insert fail: %w", err))
 		}
+		u.metrics.SuccessTotal()
+		u.metrics.ProcessDuration(t)
 	})
 }
